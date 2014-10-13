c70a2b447c7f54308b95888f76cbe7aa50726182
golivia <olivia@olivia.local>
2014-10-12 22:37:10 -0700

    initial commit with initial home screen

---
+++ tumblrApp-om/TabBarViewController.swift
@@ -0,0 +1,68 @@
+//
+//  TabBarViewController.swift
+//  tumblrApp-om
+//
+//  Created by Olivia Muesse on 10/12/14.
+//  Copyright (c) 2014 Olivia Muesse. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var composeButton: UIButton!
+    @IBOutlet weak var accountButton: UIButton!
+    @IBOutlet weak var trendingButton: UIButton!
+    
+    var homeViewController: UIViewController!
+    var searchViewController: UIViewController!
+    var activityViewController: UIViewController!
+    var feedViewController: UIViewController!
+    var accountViewController: UIViewController!
+    var composeViewController: UIViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController") as UIViewController
+        
+        searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController") as UIViewController
+        
+        accountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController") as UIViewController
+        
+        activityViewController = storyboard.instantiateViewControllerWithIdentifier("ActivityViewController") as UIViewController
+
+        
+        onHomeButton(self)
+        
+    }
+
+    @IBAction func onHomeButton(sender: AnyObject) {
+        println("a")
+        homeViewController.view.frame = containerView.frame
+        containerView.addSubview(homeViewController.view)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
